cf9c91ce5bb8e10da54f544804161f38cfc68854
Alireza Raei <alirezaraei17@gmail.com>
2022-12-31 15:14:12 +1100

    Added Throttling for authenticated and anonymous users.

--- LittleLemon/settings.py
+++ LittleLemon/settings.py
@@ -133,4 +133,12 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 5,
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle'
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '5/hour',
+        'user': '10/hour'
+    },
 }
